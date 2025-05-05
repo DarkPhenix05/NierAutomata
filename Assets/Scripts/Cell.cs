1760044f240b2c6206cc5915592b54ad8a35b91a
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-05-05 13:26:14 -0600

    Update

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,17 +2,73 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    private bool Life;
+    public bool Life = false;
+    private SpriteRenderer SRenderer;
+    private Camera cam;
 
-
+    void Start()
+    {
+        SRenderer = GetComponent<SpriteRenderer>();
+        cam = Camera.main;
+    }
 
     public void SetState(bool state)
     {
         Life = state;
+
+        if (!SRenderer)
+        {
+            SRenderer = GetComponent<SpriteRenderer>();
+        }
+        SRenderer.color = Life ? Color.white : Color.black;
+    }
+
+    private void Flip()
+    {
+        Life = !Life;
+
+        if (!SRenderer)
+        {
+            SRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        SRenderer.color = Life ? Color.white : Color.black;
     }
 
     public bool GetState()
     {
         return (Life);
+    }
+
+    private void OnMouseOver()
+    {
+        //Debug.Log("Over: " + this.gameObject.ToString());
+        if (CelularAutomat1D.Instance != null)
+        {
+            if (!CelularAutomat1D.Instance.Running && Input.GetMouseButtonDown(0))
+            {
+                Flip();
+                //Debug.Log("Flip: " + this.gameObject.ToString());
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 viewPos = cam.WorldToScreenPoint(transform.position);
+
+        if (viewPos.y < 0f || viewPos.y > Screen.height ||
+            viewPos.x < 0f || viewPos.x > Screen.width ||
+            viewPos.z < 0f) // z < 0 means behind the camera
+        {
+            gameObject.SetActive(false);
+        }
+        else if (CelularAutomat1D.Instance != null)
+        {
+            if (CelularAutomat1D.Instance.Running)
+            {
+                transform.localPosition += new Vector3(0f, 1f * Time.fixedDeltaTime, 0f);
+            }
+        }
     }
 }
--- Assets/Scripts/CelularAutomat1D.cs
+++ Assets/Scripts/CelularAutomat1D.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
+using System.Drawing;
 using UnityEngine.UI;
 using TMPro;
+using Random = UnityEngine.Random;
 
 public class CelularAutomat1D : MonoBehaviour
 {
-    public int NumberOfCells;
-    public float ActiveTime;
-    public float Sice;
+    public static CelularAutomat1D Instance;
 
     public GameObject preCell;
 
-    public List<Cell> ParrentCells = new List<Cell>();
-    public List<Cell> ChildCells = new List<Cell>();
+    public int NumberOfCells;
+    public float ActiveTime;
+    private float size;
+    public float Spacing;
+
+    public List<GameObject> AveliableCells = new List<GameObject>();
+    public List<GameObject> CurentCells = new List<GameObject>();
+
+    public List<bool> ParrentCells = new List<bool>();
+    public List<bool> ChildCells = new List<bool>();
 
     private bool stateL;
     private bool stateS;
@@ -47,74 +55,148 @@ public class CelularAutomat1D : MonoBehaviour
     public Button B111;
     private bool S111;
 
-    public void Awake()
+    public Button StartStop;
+    public bool Running = false;
+
+    private void Awake()
     {
-        Set000(true);
-
-        Set001(true);
-        Set010(true);
-        Set100(true);
-        
-        Set011(true);
-        Set101(true);
-        Set110(true);
-
-        Set111(true);
-
-
-        
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
     }
 
-    public void GenerateFirstGeneration()
+    public void Start()
     {
-        for (int i = 0; i < NumberOfCells; i++)
-        {
-            GameObject curCell = Instantiate(preCell);
-            curCell.transform.position = new Vector3((Sice*i), 0, 0);
-        }
+        Set000(Random.value < 0.5f);
+
+        Set001(Random.value < 0.5f);
+        Set010(Random.value < 0.5f);
+        Set100(Random.value < 0.5f);
+        
+        Set011(Random.value < 0.5f);
+        Set101(Random.value < 0.5f);
+        Set110(Random.value < 0.5f);
+
+        Set111(Random.value < 0.5f);
+
+        size = preCell.transform.localScale.x;
+
+        GenerateFirstGeneration();
     }
 
     public void OperateGeneration()
     {
-        for(int i = 0; i < ParrentCells.Count; i++)
+        UpdateCells();
+
+        for (int i = 0; i < ParrentCells.Count; i++)
         {
             GetNeighbors(i);
-            ChildCells[i].SetState(GenerateChild());
+            ChildCells[i].Equals(GenerateChild());
         }
 
-        for (int i = 0; i < ChildCells.Count; i++)
-        {
-            Debug.Log(ChildCells[i].GetState());
-        }
-
-        ParrentCells = ChildCells;
+        CurentCells.Clear();
     }
 
+    public void UpdateCells()
+    {
+        MoveActiveCellsUp();
+        CurentCells.Clear();
+
+        for (int i = 0; i < NumberOfCells; i++)
+        {
+            GameObject curCell = AveliableCells[GetInactive()];
+            CurentCells.Add(curCell);
+
+            curCell.transform.position = new Vector3((size * i + Spacing * i), 0, 0);
+
+            Cell tempCell = curCell.GetComponent<Cell>();
+            tempCell.Life.Equals(ChildCells[i]);
+            curCell.SetActive(true);
+
+            curCell.transform.parent.Equals(AveliableCells);
+        }
+    }
+
+    public void GetParentsCurentState()
+    {
+        for (int i = 0; i < CurentCells.Count; i++)
+        {
+            ParrentCells[i].Equals(CurentCells[i].GetComponent<Cell>().Life);
+        }
+    }
+
+    public int GetInactive()
+    {
+        for (int i = 0; i < AveliableCells.Count; i++)
+        {
+            if (!AveliableCells[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+
+        SpawnNewCell();
+        return GetInactive();
+    }
+    public void SpawnNewCell()
+    {
+        GameObject curCell = Instantiate(preCell);
+        curCell.SetActive(false);
+
+        curCell.transform.SetParent(this.gameObject.transform);
+
+        curCell.GetComponent<Cell>().SetState(Random.value < 0.5f);
+        ChildCells.Add(curCell.GetComponent<Cell>().Life);
+
+        AveliableCells.Add(curCell);
+    }
+
+    public void MoveActiveCellsUp()
+    {
+        for (int i = 0; i < AveliableCells.Count; i++)
+        {
+            if (AveliableCells[i].activeInHierarchy)
+            {
+                AveliableCells[i].transform.localPosition += new Vector3(0f, size + Spacing, 0f);
+            }
+        }
+    }
+
+    public void MoveUp(float step)
+    {
+        transform.localPosition += new Vector3(0f, step, 0f);
+    }
+    
     public void GetNeighbors(int val)
     {
-        //Previous Neighbor
-        if(val != 0) 
-        {            
-            stateL = ParrentCells[val - 1].GetState();            
+        // Left neighbor
+        if (val != 0)
+        {
+            stateL = ParrentCells[val - 1];
         }
         else
         {
-            stateL = ParrentCells[ParrentCells.Count - 2].GetState();
+            stateL = ParrentCells[ParrentCells.Count - 1];
         }
 
-        //Self
-        stateS = ParrentCells[val].GetState();
+        // Self
+        stateS = ParrentCells[val];
 
-        //Next Neighbor
+        // Right neighbor
         if (val != ParrentCells.Count - 1)
         {
-            stateL = ParrentCells[val + 1].GetState();
+            stateR = ParrentCells[val + 1];
         }
         else
         {
-            stateR = ParrentCells[0].GetState();
+            stateR = ParrentCells[0];
         }
-       
+
+        Debug.Log(val);
+        Debug.Log("  L-" + val + ": " + stateL);
+        Debug.Log("  S-" + val + ": " + stateS);
+        Debug.Log("  R-" + val + ": " + stateR);
     }
 
     public bool GenerateChild()
@@ -167,48 +249,48 @@ public class CelularAutomat1D : MonoBehaviour
     public void Set000()
     {
         S000 = !S000;
-        B000.GetComponentInChildren<TextMeshPro>().text = S000.ToString();
+        B000.GetComponentInChildren<TextMeshProUGUI>().text = S000.ToString();
     }
 
     //UN vivo
     public void Set001()
     {
         S001 = !S001;
-        B001.GetComponentInChildren<TextMeshPro>().text = S001.ToString();
+        B001.GetComponentInChildren<TextMeshProUGUI>().text = S001.ToString();
     }
     public void Set010()
     {
         S010 = !S010;
-        B010.GetComponentInChildren<TextMeshPro>().text = S010.ToString();
+        B010.GetComponentInChildren<TextMeshProUGUI>().text = S010.ToString();
     }
     public void Set100()
     {
         S100 = !S100;
-        B100.GetComponentInChildren<TextMeshPro>().text = S100.ToString();
+        B100.GetComponentInChildren<TextMeshProUGUI>().text = S100.ToString();
     }
 
     //DOS vivos
     public void Set011()
     {
         S011 = !S011;
-        B011.GetComponentInChildren<TextMeshPro>().text = S011.ToString();
+        B011.GetComponentInChildren<TextMeshProUGUI>().text = S011.ToString();
     }
     public void Set101()
     {
         S101 = !S101;
-        B101.GetComponentInChildren<TextMeshPro>().text = S101.ToString();
+        B101.GetComponentInChildren<TextMeshProUGUI>().text = S101.ToString();
     }
     public void Set110()
     {
         S110 = !S110;
-        B110.GetComponentInChildren<TextMeshPro>().text = S110.ToString();
+        B110.GetComponentInChildren<TextMeshProUGUI>().text = S110.ToString();
     }
 
     //TRES vivos
     public void Set111()
     {
         S111 = !S111;
-        B111.GetComponentInChildren<TextMeshPro>().text = S111.ToString();
+        B111.GetComponentInChildren<TextMeshProUGUI>().text = S111.ToString();
     }
 
 //Manual
@@ -216,47 +298,66 @@ public class CelularAutomat1D : MonoBehaviour
     public void Set000(bool val)
     {
         S000 = val;
-        B000.GetComponentInChildren<TextMeshPro>().text = S000.ToString();
+        B000.GetComponentInChildren<TextMeshProUGUI>().text = S000.ToString();
     }
 
     //UN vivo
     public void Set001(bool val)
     {
         S001 = val;
-        B001.GetComponentInChildren<TextMeshPro>().text = S001.ToString();
+        B001.GetComponentInChildren<TextMeshProUGUI>().text = S001.ToString();
     }
     public void Set010(bool val)
     {
         S010 = val;
-        B010.GetComponentInChildren<TextMeshPro>().text = S010.ToString();
+        B010.GetComponentInChildren<TextMeshProUGUI>().text = S010.ToString();
     }
     public void Set100(bool val)
     {
         S100 = val;
-        B100.GetComponentInChildren<TextMeshPro>().text = S100.ToString();
+        B100.GetComponentInChildren<TextMeshProUGUI>().text = S100.ToString();
     }
 
     //DOS vivos
     public void Set011(bool val)
     {
         S011 = val;
-        B011.GetComponentInChildren<TextMeshPro>().text = S011.ToString();
+        B011.GetComponentInChildren<TextMeshProUGUI>().text = S011.ToString();
     }
     public void Set101(bool val)
     {
         S101 = val;
-        B101.GetComponentInChildren<TextMeshPro>().text = S101.ToString();
+        B101.GetComponentInChildren<TextMeshProUGUI>().text = S101.ToString();
     }
     public void Set110(bool val)
     {
         S110 = val;
-        B110.GetComponentInChildren<TextMeshPro>().text = S110.ToString();
+        B110.GetComponentInChildren<TextMeshProUGUI>().text = S110.ToString();
     }
 
     //TRES vivos
     public void Set111(bool val)
     {
         S111 = val;
-        B111.GetComponentInChildren<TextMeshPro>().text = S111.ToString();
+        B111.GetComponentInChildren<TextMeshProUGUI>().text = S111.ToString();
     }
+
+    public void GenerateFirstGeneration()
+    {
+        for (int i = 0; i < NumberOfCells; i++)
+        {
+            GameObject curCell = Instantiate(preCell);
+            curCell.transform.SetParent(this.gameObject.transform);
+            curCell.transform.position = new Vector3((size * i + Spacing * i), 0, 0);
+
+            curCell.GetComponent<Cell>().SetState(Random.value < 0.5f);
+
+            AveliableCells.Add(curCell);
+
+            Debug.Log(curCell.gameObject.activeInHierarchy);
+            curCell.SetActive(true);
+            Debug.Log(curCell.gameObject.activeInHierarchy);
+        }
+    }
+
 }
