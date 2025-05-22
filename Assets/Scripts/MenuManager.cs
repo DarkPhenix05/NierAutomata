2a77454d2ff09037e435fdafd412dd84f0f0857e
JorgeYTP <152333387+DarkPhenix05@users.noreply.github.com>
2025-05-22 14:52:50 -0600

    MenuStart

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _playerUI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PauseMenu()
+    {
+        if(!_pauseMenu || !_playerUI)
+        {
+            return;
+        }
+        
+        if(!_pauseMenu.activeInHierarchy)
+        {
+            _pauseMenu.SetActive(true);
+            _playerUI.SetActive(false);
+        }
+        else
+        {
+            _pauseMenu.SetActive(false);
+            _playerUI.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+
+        Debug.Log("EXIT BUTTON ACTION");
+        
+#endif
+        Application.Quit();
     }
 }
